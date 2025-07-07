02296998b3f06d55c751f18f8f572cb30cde2086
diki <muliaandiki@gmaiil.com>
2025-07-08 03:15:36 +0700

    fixed:

--- app/core/components/promotion-app.tsx
+++ app/core/components/promotion-app.tsx
@@ -7,7 +7,7 @@ import { PromotionSchemaProps } from "@/app/types/props";
 const PromotionApp: React.FC<PromotionSchemaProps> = ({ data }) => {
   return (
     <Container className="w-full min-h-full">
-      <Container className="bg-[var(--shape-parent)] flex justify-between items-center w-full border rounded-md p-3 relative overflow-hidden z-0 ">
+      <Container className="bg-[var(--shape-parent)] flex justify-between items-center w-full  rounded-md p-3 relative overflow-hidden z-0 ">
         <PromotionShapeApp />
         <Container className="flex justify-start flex-col">
           <Text className="md:text-2xl text-lg font-bold w-full ">
@@ -15,7 +15,7 @@ const PromotionApp: React.FC<PromotionSchemaProps> = ({ data }) => {
           </Text>
           <Text className="text-3xl md:text-lg font-bold">{data.desk}</Text>
         </Container>
-        <Container className="flex border w-full ">
+        <Container className="flex  w-full ">
           <Image
             alt="Foto"
             src={data.image}

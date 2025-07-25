1df1a501f9e5fe969f9430c1b806a48fde38f369
diki <muliaandiki@gmaiil.com>
2025-07-25 10:07:13 +0700

    fixed: oraginer remove change to brands + validation document model

--- app/(pages)/(private)/user/profile/edit-profile/update-role/_container/update-role.tsx
+++ app/(pages)/(private)/user/profile/edit-profile/update-role/_container/update-role.tsx
@@ -8,12 +8,16 @@ import { formUpdateRoleSchema } from "@/app/types/form";
 import { useState } from "react";
 import { useUpdateRole } from "@/app/hooks/mutation/auth/useUpdateRole";
 import { useAlert } from "@/app/hooks/alert/costum-alert";
+import { Label } from "@/app/ui/label";
+import { useAppSelector } from "@/app/hooks/dispatch/dispatch";
 
 const UpdateRoleChildren: React.FC = () => {
+  const currentId = useAppSelector((state) => state.auth.currentUser?.user._id);
   const [formUpdateRole, setFormUpdateRole] = useState<formUpdateRoleSchema>({
-    userId: "",
+    userId: currentId,
     brand: "",
     document: {},
+    description: "",
   });
   const alert = useAlert();
 
@@ -34,15 +38,29 @@ const UpdateRoleChildren: React.FC = () => {
     }
     return update(formUpdateRole);
   };
+
   const { isMobile } = useIsMobile();
   return (
     <Container className="w-full h-full ">
       {isMobile && (
         <ProfileLayout>
-          <Container className="flex justify-center items-center">
-            <Text className="text-lg md:text-4xl font-bold">
+          <Container className="flex justify-center items-center flex-col">
+            <Text className="text-lg md:text-4xl font-bold my-6">
               Upgrade - Role
             </Text>
+            <Container className=" w-full max-w-4/5 mx-auto my-6  gap-2">
+              <Label className="font-bold text-lg md:text-4xl">AA</Label>
+              <Input
+                placeholder="Brand"
+                value={formUpdateRole.brand}
+                onChange={(e) =>
+                  setFormUpdateRole((prev) => {
+                    const newObj = { ...prev, brand: e.target.value };
+                    return newObj;
+                  })
+                }
+              />
+            </Container>
           </Container>
         </ProfileLayout>
       )}

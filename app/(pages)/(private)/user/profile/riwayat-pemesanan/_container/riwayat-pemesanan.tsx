5b0d1aef1f67dfc846be0a00463050c0eb7e06b9
diki <muliaandiki@gmaiil.com>
2025-08-25 23:53:01 +0700

    feat: referensi desain for app in figma & setup state component for app

--- app/(pages)/(private)/user/profile/riwayat-pemesanan/_container/riwayat-pemesanan.tsx
+++ app/(pages)/(private)/user/profile/riwayat-pemesanan/_container/riwayat-pemesanan.tsx
@@ -4,15 +4,59 @@ import { useIsMobile } from '@/app/hooks/Mobile/use-mobile';
 import ProfileLayout from '@/app/core/layouts/profile-layout';
 import DesktopBlock from '@/app/components/desktop-block';
 import { Text } from '@/app/ui/Text';
+import { useState } from 'react';
+import { Button } from '@/app/ui/button';
 
 const RiwayatPemesananChildren = () => {
   const { isMobile } = useIsMobile();
+  const [isState, setIsState] = useState<'Active' | 'Complate' | 'Cancel'>('Active');
   return (
     <Container as="main" className="w-full h-full">
       {isMobile && (
         <ProfileLayout>
-          <Container className="flex justify-center items-center">
-            <Text>Setup Page</Text>
+          <Container className="flex justify-center items-center flex-col  ">
+            <Text className="font-extrabold text-3xl">Riwayat Pembelian</Text>
+            <Text className="my-2 text-lg font-semibold">Deskripsi</Text>
+            <Container className="flex justify-around items-center  w-full">
+              <Button
+                className="font-semibold"
+                variant="outline"
+                onClick={() => setIsState('Active')}
+              >
+                Active
+              </Button>
+              <Button
+                className="font-semibold"
+                variant="outline"
+                onClick={() => setIsState('Cancel')}
+              >
+                Cancel
+              </Button>
+              <Button
+                className="font-semibold"
+                variant="outline"
+                onClick={() => setIsState('Complate')}
+              >
+                Berhasil
+              </Button>
+            </Container>
+            <Container>
+              {isState === 'Active' && (
+                <Container>
+                  <Text>Active State</Text>
+                </Container>
+              )}
+              {isState === 'Cancel' && (
+                <Container>
+                  <Text>Cancel State</Text>
+                </Container>
+              )}
+              {isState === 'Complate' && (
+                <Container>
+                  <Text>Complate State</Text>
+                </Container>
+              )}
+            </Container>
           </Container>
         </ProfileLayout>
       )}
